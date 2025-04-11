3d8c36d91acffb33cd6382364685db2a2a2c3687
Sergeeva <allasergeevna0521@gmail.com>
2025-04-11 16:02:48 +0300

    home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Accordion from "@/components/accordion";
+import { HomeBG } from "@/components/homeBG/ihdex";
+import { Slider } from "@/components/slider";
 
 export default function Home() {
   return (
     <div>
-      {" "}
+      <HomeBG />
+      <Slider />
       <Accordion />
     </div>
   );
