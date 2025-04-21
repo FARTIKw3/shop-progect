68b4d744f4bcb3609f2b9a9212e2fb5cebe18ee7
Ramazan <ramazandjaparov@gmail.com>
2025-04-21 23:07:10 +0600

    .

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -3,9 +3,11 @@ import { CatalogTitle } from "@/components/catalogTitle";
 import { CategorySlider } from "@/components/categorySlider";
 import { LaptopImage } from "@/components/laptopImage";
 import { Slider } from "@/components/slider";
+import { IProduct, StrapiType } from "@/interfaces/strapiData";
 
 export default async function Catalog() {
-  const { data } = await fetchProducts("image");
+  const response = await fetchProducts<StrapiType<IProduct[]>>("image");
+  const data: IProduct[] = response.data;
   return (
     <div>
       <LaptopImage />
