b42a31d53b73f9f99fe5368f175edf469c7f5961
Ramazan <ramazandjaparov@gmail.com>
2025-04-22 16:11:20 +0600

    feature/auth completed authorization checks added

--- actions/login.action.ts
+++ actions/login.action.ts
@@ -2,20 +2,30 @@
 
 import { postLogin } from "@/api/strapi";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export const loginActions = async (formData: FormData) => {
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
 
-  const userData = await postLogin({
-    identifier: email,
-    password: password,
-  });
-  if (userData) {
-    const cookiesData = await cookies();
-    cookiesData.set("jwt", userData.jwt, {
+  try {
+    const userData = await postLogin({
+      identifier: email,
+      password: password,
+    });
+
+    if (!userData || !userData.jwt) {
+      throw new Error("Invalid credentials");
+    }
+
+    const cookiesData = cookies();
+    (await cookiesData).set("jwt", userData.jwt, {
       httpOnly: true,
       maxAge: 60 * 60 * 24 * 7,
     });
+
+    redirect("/");
+  } catch (error: any) {
+    return { error: "Неверный email или пароль" };
   }
 };
--- components/login/index.tsx
+++ components/login/index.tsx
@@ -1,67 +1,70 @@
 "use client";
+
+import { useFormState } from "react-dom";
 import clsx from "clsx";
 import styles from "./style.module.css";
 import { loginActions } from "@/actions/login.action";
+
 interface ILoginProps {
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
 }
+
 export const Login = ({ isOpen, setIsOpen }: ILoginProps) => {
   const closeModal = () => setIsOpen(false);
+  const [state, formAction] = useFormState(loginActions, null);
 
   return (
-    <>
+    <div
+      className={clsx(styles.modal, isOpen && styles.active)}
+      onClick={closeModal}
+    >
       <div
-        className={clsx(styles.modal, isOpen && styles.active)}
-        onClick={closeModal}
+        className={clsx(styles.content, isOpen && styles.active)}
+        onClick={(e) => e.stopPropagation()}
       >
-        <div
-          className={clsx(styles.content, isOpen && styles.active)}
-          onClick={(e) => e.stopPropagation()}
-        >
-          <div>
-            <div className={styles.titleContainer}>
-              <h1 className={styles.title}>Вход</h1>
-              <span className={styles.span}>Добро пожаловать в наш Shop!</span>
-            </div>
-            <form action={loginActions} className={styles.form}>
-              <div className={styles.labelCont}>
-                <label htmlFor="email" className={styles.label}>
-                  Enter your email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className={styles.input}
-                  placeholder="E-mail"
-                />
-              </div>
-              <div className={styles.labelCont}>
-                <label htmlFor="password" className={styles.label}>
-                  Enter your password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  className={styles.input}
-                  name="password"
-                />
-              </div>
-              <div className={styles.inputContainer}></div>
-              <div className={styles.btnblock}>
-                <button
-                  className={styles.formBtn}
-                  onClick={closeModal}
-                  type="submit"
-                >
-                  Вход
-                </button>
-              </div>
-            </form>
+        <div>
+          <div className={styles.titleContainer}>
+            <h1 className={styles.title}>Вход</h1>
+            <span className={styles.span}>Добро пожаловать в наш Shop!</span>
           </div>
+          <form action={formAction} className={styles.form}>
+            {state?.error && (
+              <div className={styles.errorMessage}>{state.error}</div>
+            )}
+            <div className={styles.labelCont}>
+              <label htmlFor="email" className={styles.label}>
+                Enter your email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className={styles.input}
+                placeholder="E-mail"
+                required
+              />
+            </div>
+            <div className={styles.labelCont}>
+              <label htmlFor="password" className={styles.label}>
+                Enter your password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className={styles.input}
+                name="password"
+                required
+              />
+            </div>
+            <div className={styles.btnblock}>
+              <button className={styles.formBtn} type="submit">
+                Вход
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-    </>
+    </div>
   );
 };
